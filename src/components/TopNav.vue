fa18aa7dd3806ed4732b8aa9af50abcc06dda65a
Robbie Carlton <robbie.carlton@gmail.com>
2020-11-17 18:30:22 -0700

    Style logout menu

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -5,7 +5,10 @@
     <RightChevronIcon v-if="showSubtitle" class="chevron" />
     <div v-if="showSubtitle" class="sub-title">{{ subTitle }}</div>
     <div class="right">
-      <button @click="logout" class="logout">Logout</button>
+      <div class="owner" @click="toggleMenu">Your HP <DownTriangleIcon class='down-triangle' /></div>
+      <div v-if="menuOpen" class="menu">
+        <button @click="logout" class="menu-item">Logout</button>
+      </div>
     </div>
   </section>
 </template>
@@ -13,11 +16,13 @@
 <script>
 import { eraseHpAdminKeypair } from 'src/utils/keyManagement'
 import RightChevronIcon from 'components/icons/RightChevronIcon.vue'
+import DownTriangleIcon from 'components/icons/DownTriangleIcon.vue'
 
 export default {
   name: 'TopNav',
   components: {
-    RightChevronIcon
+    RightChevronIcon,
+    DownTriangleIcon
   },
   props: {
     breadcrumbs: {
@@ -25,8 +30,17 @@ export default {
       default: [{}, {}]
     }
   },
+  data () {
+    return {
+      menuOpen: false
+    }
+  },
   methods: {
+    toggleMenu () {
+      this.menuOpen = !this.menuOpen
+    },
     logout () {
+      console.log('lougout!!')
       eraseHpAdminKeypair()
       localStorage.removeItem('auth_token')
       this.$router.push('/login')
@@ -76,11 +90,30 @@ export default {
 }
 .right {
   margin-left: auto;  
+  margin-right: 80px;
 }
-.logout {
-  border: none;
-  background-color: transparent;
+.owner {
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 19px;
   color: #313C59;
-  cursor: pointer;
+}
+.down-triangle {
+  margin-left: 3px;
+}
+.menu {
+  background: #FFFFFF;
+  border: 1px solid #606C8B;
+  border-radius: 2px;
+  font-size: 14px;
+  line-height: 19px;
+  color: #606C8B;
+  padding: 5px 0px;
+}
+.menu-item {
+  padding: 0px 6px;
+}
+.menu-item:hover {
+  background-color: rgba(176, 236, 240, 0.72);
 }
 </style>
