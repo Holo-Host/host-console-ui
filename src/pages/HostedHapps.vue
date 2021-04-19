1354a70550b53a4973b0ccd578df933a93bd9088
Robbie Carlton <robbie.carlton@gmail.com>
2021-04-19 13:58:43 -0600

    resize and position filter close icon

--- src/pages/HostedHapps.vue
+++ src/pages/HostedHapps.vue
@@ -6,7 +6,7 @@
       </div>
       <div class='filter'>
         <input v-model="filter" class="filter-input" />
-        <ExIcon v-if="filter.length > 0" @click="clearFilter" class='ex-icon' />
+        <ExIcon v-if="filter.length > 0" @click="clearFilter" size='15' class='ex-icon' />
       </div>
       <div class="label">
         Sort by:&nbsp;
@@ -101,8 +101,8 @@ export default {
 }
 .ex-icon {
   position: absolute;
-  top: 5px;
-  right: -7px;
+  top: 8px;
+  right: 6px;
 }
 .sort {
   appearance: none;
