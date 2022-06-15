946ad1f36fb603dcd065d24f0add097adf980ed5
Mateusz Rybczonek <m.rybczonek@gmail.com>
2022-06-15 12:55:44 +0200

    feat(overlay): implement single overlay logic

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <router-view />
+	<TheOverlay ref="theOverlay" />
+	<router-view />
 </template>
 
-<script>
-export default {
-  name: 'App'
+<script setup>
+import { ref, onMounted, onUnmounted } from "vue";
+import TheOverlay from "./components/TheOverlay";
+import { addObserver, ENotification, removeObserver } from './utils/notifications'
+
+const theOverlay = ref()
+
+function showBusyOverlay(object) {
+	theOverlay.value?.show(object)
 }
+
+function hideBusyOverlay() {
+	theOverlay.value?.hide()
+}
+
+onMounted(() => {
+	addObserver(ENotification.showBusyState, showBusyOverlay)
+	addObserver(ENotification.hideBusyState, hideBusyOverlay)
+})
+
+onUnmounted(() => {
+	removeObserver(ENotification.showBusyState, showBusyOverlay)
+	removeObserver(ENotification.hideBusyState, hideBusyOverlay)
+})
 </script>
 
 <style>
 html {
   overflow-y: scroll;
 }
+
 body, html {
   height: 100%;
 }
+
 #app {
   font-family: 'Nunito Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   background-color: #fafcfe;
-  height: 100%;
+  height: 100vh;
   display: flex;
   flex-direction: column;
 }
