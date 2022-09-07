602c4c111b5c9a5a7c342231a8c5b1a369d3e5a5
jetttech <ldjetton21@gmail.com>
2022-09-07 18:38:18 -0500

    force profile defaults

--- src/pages/SettingsPage.vue
+++ src/pages/SettingsPage.vue
@@ -6,8 +6,8 @@
     />
 
     <SettingsHolofuelSection
-      :nickname="settings.holofuel.nickname"
-      :agent-key="settings.holofuel.agentAddress"
+      :nickname="settings.holofuel?.nickname || ''"
+      :agent-key="settings.holofuel?.agentAddress || null"
       class="settings__holofuel-section"
     />
   </PrimaryLayout>
