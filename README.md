fb1ab43aee5ddb6ce974cbf68d47c1fc52968640
Mateusz Rybczonek <m.rybczonek@gmail.com>
2022-09-14 14:41:32 +0200

    feat: update Readme and router

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # host-console-ui
 
 ## Git Submodules
-This project contains git submodules. By default when fetching or cloaning git will NOT include submodules (unless you include the --recurse-submodules parameters for example.
+This project contains git submodules, remember to initialize submodules after cloning.
 
 ```
-git clone --recurse-submodules https://github.com/Holo-Host/publisher-portal.git
+git clone https://github.com/Holo-Host/host-console-ui.git
+```
+
+Initialize submodules:
+
+```
+git submodule init
 ```
 
 To fetch the submodules for an existing branch you can execute:
@@ -16,13 +22,17 @@ git submodule update --remote
 The submodules default to the develop branch.
 
 ## Project setup
+
+Install required packages
 ```
-yarn install
+yarn
 ```
 
-Copy the .env.example file to .env, and modify values to taste.
+Create the .env file in the root folder and add following value.
 
 ### Start UI server using real HoloPort as backend
+Set `VUE_APP_HOLOPORT_URL` value in .env file to the HoloPort URL.
+
 ```
 VUE_APP_HOLOPORT_URL=your_holoport_url_here
 ```
@@ -30,56 +40,55 @@ VUE_APP_HOLOPORT_URL=your_holoport_url_here
 e.g.
 
 ```
-VUE_APP_HOLOPORT_URL=https://00000000000000000000000000000000000000000000000000.holohost.dev yarn serve
+VUE_APP_HOLOPORT_URL=https://00000000000000000000.holohost.dev
+```
+
+Start UI server
+```
+yarn serve
 ```
 
 Using this command, all requests to `localhost:8080/api/` and `localhost:8080/holochain-api/` are forwarded to the holoport. This is really helpful for development/testing.
 
-### Start UI server for development
-```
-yarn run serve
-```
-
-You will mostly want to run this along with the next command
+If you want to run it against mocked data you will need to run it as below.
 
 ### Start mock HPOS API server for development
+Remove `VUE_APP_HOLOPORT_URL` value from .env file.
+Set `VUE_APP_HPOS_PORT=4567` in .env file.
+
+Run mock HPOS API server
 ```
-yarn run start-mock-hpos-api
+yarn start-mock-hpos-api
+```
+
+Start UI server in another terminal
+```
+yarn serve
 ```
 
 The login email and password for this server are in the package.json script
 `test@test.com`
 and
-`asasasas`
-
-### Start both UI server and mock API server with one command
-```
-yarn run start-ui-and-mock
-```
-
+`asasas`
 
 ### Run tests
 ```
-yarn run test
+yarn test
 ```
 
-Note: you must have a .env file with `VUE_APP_HPOS_PORT` for tests to pass.
+Note: you must have a .env file with `VUE_APP_HPOS_PORT` for tests to pass as it uses the mocked HPOS API server.
 
 ### Compiles and minifies for production
 ```
-yarn run build
+yarn build
 ```
 
 ### Lints and fixes files
 ```
-yarn run lint
+yarn lint
 ```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
-
 ### Storybook
-
 This project contains Storybook stories. To run storybook in the development mode:
 
 1. Run storybook `yarn storybook`
@@ -87,3 +96,6 @@ This project contains Storybook stories. To run storybook in the development mod
 
 Storybook is automatically deployed to `https://holo-host.github.io/host-console-ui` on every merge to the
 `develop` branch.
+
+### User authentication mechanism
+See [Token authentication](https://hackmd.io/Evi5CVFqTI22UD8_OrDgYA?view) description.
--- src/router.js
+++ src/router.js
@@ -3,22 +3,23 @@ import EarningsInvoices from 'pages/EarningsInvoices.vue'
 import HappDetails from 'pages/HappDetails.vue'
 import HostedHapps from 'pages/HostedHapps.vue'
 import HostingPreferences from 'pages/HostingPreferences.vue'
-import Login from 'pages/Login.vue'
-import Settings from 'pages/Settings.vue'
 import DashboardPage from 'src/pages/DashboardPage.vue'
-import { checkHpAdminKeypair } from 'src/utils/keyManagement'
+import LoginPage from 'src/pages/LoginPage.vue'
+import SettingsPage from 'src/pages/SettingsPage.vue'
 import { createWebHistory, createRouter } from 'vue-router'
+import { kAuthTokenLSKey } from '@/constants'
 
-export const routes = [
-  {
+export const kRoutes = {
+  login: {
     path: '/login',
     name: 'Login',
-    component: Login,
+    component: LoginPage,
     meta: {
       guest: true
     }
   },
-  {
+
+  dashboard: {
     path: '/dashboard',
     name: 'Dashboard',
     component: DashboardPage,
@@ -26,7 +27,8 @@ export const routes = [
       requiresAuth: true
     }
   },
-  {
+
+  happs: {
     path: '/happs',
     name: 'HostedHapps',
     component: HostedHapps,
@@ -34,7 +36,8 @@ export const routes = [
       requiresAuth: true
     }
   },
-  {
+
+  happ: {
     path: '/happ/:id',
     name: 'HappDetails',
     component: HappDetails,
@@ -42,15 +45,17 @@ export const routes = [
       requiresAuth: true
     }
   },
-  {
+
+  accountSettings: {
     path: '/settings',
     name: 'Settings',
-    component: Settings,
+    component: SettingsPage,
     meta: {
       requiresAuth: true
     }
   },
-  {
+
+  earnings: {
     path: '/earnings',
     name: 'Earnings',
     component: Earnings,
@@ -58,7 +63,8 @@ export const routes = [
       requiresAuth: true
     }
   },
-  {
+
+  invoices: {
     path: '/earnings/invoices',
     name: 'EarningsInvoices',
     component: EarningsInvoices,
@@ -66,7 +72,8 @@ export const routes = [
       requiresAuth: true
     }
   },
-  {
+
+  hostingPreferences: {
     path: '/preferences',
     name: 'HostingPreferences',
     component: HostingPreferences,
@@ -74,33 +81,38 @@ export const routes = [
       requiresAuth: true
     }
   },
-  {
+
+  default: {
     path: '/',
     redirect: '/dashboard'
   }
-]
+}
 
 // used in loginFlow.integration.test.js
 export const routerFactory = () => {
   const router = createRouter({
     history: createWebHistory(),
-    routes
+    routes: [
+      kRoutes.accountSettings,
+      kRoutes.dashboard,
+      kRoutes.default,
+      kRoutes.earnings,
+      kRoutes.happ,
+      kRoutes.happs,
+      kRoutes.hostingPreferences,
+      kRoutes.invoices,
+      kRoutes.login
+    ]
   })
 
   router.beforeEach((to, from, next) => {
     if (to.matched.some((record) => record.meta.requiresAuth)) {
       // page only visible when logged in
 
-      // isAuthed is true if the last keypair we generated was good. It persists across sessions.
-      // checkHpAdminKeypair checks if we have a keypair in *this* session. If we don't, then we remove isAuthed
-      // another way to handle this would be to store isAuthed in app state not local storage. Then we wouldn't need this line.
-      if (!checkHpAdminKeypair()) {
-        localStorage.removeItem('isAuthed')
-      }
-
-      if (localStorage.getItem('isAuthed') === null) {
+      // Existence of AuthToken in localStorage is equal to user logged in
+      if (localStorage.getItem(kAuthTokenLSKey) == null) {
         next({
-          name: 'Login',
+          name: kRoutes.login.name,
           params: { nextUrl: to.fullPath }
         })
       } else {
@@ -108,10 +120,10 @@ export const routerFactory = () => {
       }
     } else if (to.matched.some((record) => record.meta.guest)) {
       // page only visible when *not* logged in
-      if (localStorage.getItem('isAuthed') === null) {
+      if (localStorage.getItem(kAuthTokenLSKey) == null) {
         next()
       } else {
-        next({ name: 'Dashboard' })
+        next({ name: kRoutes.dashboard.name })
       }
     } else {
       // publicly visible page
