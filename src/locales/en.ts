1e95c6b99765d682f2c6b508633845795776193c
Mateusz Rybczonek <m.rybczonek@gmail.com>
2023-11-10 08:47:45 +0100

    feat(translations): update no hApps translations

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -49,8 +49,8 @@ export default {
   },
   hosted_happs: {
     hosted_for: 'Hosted for',
-    no_active_happs: 'You’re not hosting any hApps',
-    no_inactive_happs: 'You do not have any inactive hApps',
+    no_active_happs: 'You have not enabled any hApps for hosting',
+    no_inactive_happs: 'You have not disabled any hApps',
     no_filtered_happs: 'No hApps match your search',
     title: 'Top Hosted hApps'
   },
