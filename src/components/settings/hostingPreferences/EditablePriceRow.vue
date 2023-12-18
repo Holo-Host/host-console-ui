7834d6f3a727b02a2e6c710da902e300cda62348
Mateusz Rybczonek <m.rybczonek@gmail.com>
2023-12-18 10:00:22 +0100

    feature(paid-hosting-wizard): update EditablePriceRow component

--- src/components/settings/hostingPreferences/EditablePriceRow.vue
+++ src/components/settings/hostingPreferences/EditablePriceRow.vue
@@ -1,31 +1,36 @@
 <script setup lang="ts">
 import BaseInput from '@uicommon/components/BaseInput.vue'
 import { EInputType } from '@uicommon/types/ui'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import CircledExIcon from '../../icons/CircledExIcon.vue'
 import FilledCheckIcon from '../../icons/FilledCheckIcon.vue'
 import PencilIcon from '../../icons/PencilIcon.vue'
 
 const props = withDefaults(
   defineProps<{
+    type?: EInputType
     label: string
-    value: number | string
+    value: number | string | null
     unit: string
     prop: string
     isDisabled?: boolean
+    labelAlign?: 'left' | 'center'
   }>(),
   {
-    isDisabled: false
+    type: EInputType.text,
+    isDisabled: false,
+    labelAlign: 'center'
   }
 )
 
 const emit = defineEmits(['update:value'])
 
 const isEditing = ref(false)
+const isValid = ref(true)
 const editedValue = ref(props.value)
 
 function edit(): void {
-  editedValue.value = `${props.value}`
+  editedValue.value = props.value === null ? '' : `${props.value}`
   isEditing.value = true
 }
 
@@ -38,45 +43,64 @@ function cancel(): void {
   editedValue.value = `${props.value}`
   isEditing.value = false
 }
+
+watch(
+  () => editedValue.value,
+  (value) => {
+    isValid.value = value && value >= 0
+  }
+)
 </script>
 
 <template>
-    <div class="editable-price-row__editable-value">
-      <span  class="editable-price-row__label">{{ label }}</span>
-      <span  v-if="!isEditing" class="editable-price-row__readonly_value">{{ editedValue }}</span>
-      <BaseInput
-        v-if="isEditing"
-        v-model="editedValue"
-        :input-type="EInputType.text"
-        placeholder=""
-        name="edited-value"
-        class="editable-price-row__editable-value-input"
-      />
+  <div class="editable-price-row__editable-value">
+    <span
+      class="editable-price-row__label"
+      :class="{ 'editable-price-row__label--text-left': props.labelAlign === 'left' }"
+    >
+      {{ label }}
+    </span>
+    <span
+      v-if="!isEditing"
+      class="editable-price-row__readonly_value"
+    >
+      {{ editedValue }}
+    </span>
+    <BaseInput
+      v-if="isEditing"
+      v-model="editedValue"
+      :is-valid="isValid"
+      :input-type="props.type"
+      placeholder=""
+      name="edited-value"
+      class="editable-price-row__editable-value-input"
+    />
 
-      <FilledCheckIcon
-        v-if="isEditing"
-        class="editable-price-row__button"
-        data-testid="save-button"
-        role="button"
-        @click="save"
-      />
+    <FilledCheckIcon
+      v-if="isEditing"
+      class="editable-price-row__button"
+      data-testid="save-button"
+      role="button"
+      :class="{ 'editable-price-row__button--disabled': !isValid }"
+      @click="save"
+    />
 
-      <CircledExIcon
-        v-if="isEditing"
-        class="editable-price-row__button"
-        data-testid="cancel-button"
-        role="button"
-        @click="cancel"
-      />
-      <span class="editable-price-row__unit">{{ props.unit }}</span>
-      <PencilIcon
-        v-if="!isEditing"
-        role="button"
-        class="editable-price-row__editable-value-icon"
-        :class="{ 'disabled': props.isDisabled }"
-        @click="edit"
-      />
-    </div>
+    <CircledExIcon
+      v-if="isEditing"
+      class="editable-price-row__button"
+      data-testid="cancel-button"
+      role="button"
+      @click="cancel"
+    />
+    <span class="editable-price-row__unit">{{ props.unit }}</span>
+    <PencilIcon
+      v-if="!isEditing"
+      role="button"
+      class="editable-price-row__editable-value-icon"
+      :class="{ 'disabled': props.isDisabled }"
+      @click="edit"
+    />
+  </div>
 </template>
 
 <style lang="scss" scoped>
@@ -110,6 +134,12 @@ function cancel(): void {
   &__button {
     margin-left: 5px;
     cursor: pointer;
+
+    &--disabled {
+      opacity: 0.3;
+      cursor: not-allowed;
+      pointer-events: none;
+    }
   }
 
   &__unit {
@@ -123,6 +153,10 @@ function cancel(): void {
     margin-right: 16px;
     color: var(--grey-color);
     font-weight: 700;
+
+    &--text-left {
+      text-align: left;
+    }
   }
 
   &__readonly_value {
